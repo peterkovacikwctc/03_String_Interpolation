be2b7f4c142336cd2f0978056d09848b56f9706a
Peter Kovacik <peterkovacik@Peters-MBP.lan>
2020-09-07 22:15:04 -0500

    parse file and display weekly report

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ namespace SleepData
             Console.WriteLine("Enter anything else to quit.");
             // input response
             string resp = Console.ReadLine();
+            var file = "data.txt";
 
             if (resp == "1")
             {
@@ -34,7 +35,7 @@ namespace SleepData
                 Random rnd = new Random();
 
                 // create file
-                StreamWriter sw = new StreamWriter("data.txt");
+                StreamWriter sw = new StreamWriter(file);
                 // loop for the desired # of weeks
                 while (dataDate < dataEndDate)
                 {
@@ -55,8 +56,49 @@ namespace SleepData
             }
             else if (resp == "2")
             {
-                // TODO: parse data file
+                if (File.Exists(file))
+                {
+                    StreamReader sr = new StreamReader(file);
+                    Console.WriteLine("");
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        string[] arr = line.Split(',');
 
+                        // date information (before splitting with comma)
+                        string date = arr[0];
+                        string[] dateArray = date.Split('/');
+                        DateTime thisDate = Convert.ToDateTime(date);
+
+                        // weekly sleep hours (after splitting with comma)
+                        string weeklyHours = arr[1];
+                        
+                        // array of hours slept each day
+                        string[] sleepEachDay = weeklyHours.Split('|');
+
+                        // compute total hours of sleep
+                        int totalHours = 0;
+                        for (int i = 0; i < 7; i++) {
+                            totalHours += int.Parse(sleepEachDay[i]);
+                        }
+
+                        // compute average number of hours
+                        double averageHours = totalHours / 7.0;
+                        
+                        //display chart
+                        Console.WriteLine("Week of " + thisDate.ToString("MMM, dd, yyyy"));
+                        Console.WriteLine($"{"Su",3}{"Mo",3}{"Tu",3}{"We",3}{"Th",3}{"Fr",3}{"Sa",3}{"Tot",4}{"Avg",4}");
+                        Console.WriteLine($"{"--",3}{"--",3}{"--",3}{"--",3}{"--",3}{"--",3}{"--",3}{"---",4}{"---",4}");
+                        Console.WriteLine($"{sleepEachDay[0],3}{sleepEachDay[1],3}{sleepEachDay[2],3}{sleepEachDay[3],3}{sleepEachDay[4],3}{sleepEachDay[5],3}{sleepEachDay[6],3}{totalHours,4}{averageHours,4:n1}");
+                        Console.WriteLine("");
+                    }
+                    sr.Close();
+                    Console.WriteLine("");
+                }
+                else
+                {
+                    Console.WriteLine("The file '" + file + "' does not exist.");
+                }
             }
         }
     }
